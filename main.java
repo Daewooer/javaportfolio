d63115dd0d938a4acb2614bce459aa8ca7b3f4fe
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-06-02 15:25:54 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main (String args[]) {
+        int MyAge = 12;
+        String MyName = "Kostas Zafolias";
+        char MyChar = 'K';
+        String MyLanguage = "I know HTML and CSS , Javascript , Java , Python , GO and SQL";        
+        //variables 
+        Scanner myObj = new Scanner(System.in);
+    System.out.println("Hello, My name is Kostas and this is my portfolio!");
+    System.out.println("Are you ready?");
+    String Ready = myObj.nextLine();
+    if (Ready.equals("yes")) {
+        System.out.println ("Hi, I am Kostas and I am an 12 year old and I am programming from December 2020.");
+        System.out.println("I know HTML and CSS , Javascript , Java , Python , GO and SQL");
+        System.out.println("I have been offered a job from Semrush");
+    } // end of 
+    else {
+        System.out.println ("oh well");
+    } // end of if-else
+    }
+    }
